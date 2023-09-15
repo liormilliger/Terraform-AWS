d5b517c32f1e288e736b5f82c6df910e0db7ab3c
liorm <lior@email.com>
2023-09-15 06:38:38 +0300

    adding vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,89 @@
+resource "aws_vpc" "liorm-TF-easy" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "liorm-TF-easy"
+  }
+}
+
+resource "aws_subnet" "liorm-TF-easy-us-east-1a" {
+  vpc_id     = aws_vpc.liorm-TF-easy.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "liorm-TF-easy-us-east-1a"
+  }
+}
+
+resource "aws_subnet" "liorm-TF-easy-us-east-1b" {
+  vpc_id     = aws_vpc.liorm-TF-easy.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "liorm-TF-easy-us-east-1b"
+  }
+}
+
+resource "aws_internet_gateway" "liorm-TF-easy-igw" {
+  vpc_id = aws_vpc.liorm-TF-easy.id
+  tags = {
+    Name = "liorm-TF-easy-igw"
+  }
+}
+
+
+resource "aws_route_table" "liorm-TF-easy-RT" {
+  vpc_id = aws_vpc.liorm-TF-easy.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.liorm-TF-easy-igw.id
+  }
+  tags = {
+    Name = "liorm-TF-easy-RT" 
+  }
+}
+
+resource "aws_route_table_association" "liorm-TF-easy-pub-assoc-1a" {
+    subnet_id = "liorm-TF-easy-us-east-1a"
+    route_table_id = "liorm-TF-easy-RT"
+}
+
+resource "aws_route_table_association" "liorm-TF-easy-pub-assoc-1b" {
+    subnet_id = "liorm-TF-easy-us-east-1b"
+    route_table_id = "liorm-TF-easy-RT"
+}
+
+resource "aws_security_group" "liorm-TF-easy-SG" {
+  name        = "liorm-TF-easy-SG"
+  description = "Allow incoming HTTP traffic from your IP"
+
+  // Ingress rule (incoming traffic)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["89.138.129.95/32"]
+  }
+
+  // Egress rule (outgoing traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "liorm-tf-key" {
+  key_name   = "liorm-tf-key"
+  public_key = file("~/.ssh/liorm-tf-key_rsa.pub")
+}
