3e3e9c6ddefc2d63e20a2b92904bc3fa4ece85c2
liorm <lior@email.com>
2023-09-16 17:28:58 +0300

    adding backend s3

--- providers.tf
+++ providers.tf
@@ -1,9 +1,18 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "liorm-statelock"
+    key    = "data/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-liorm-state-table"
   }
 }
 
