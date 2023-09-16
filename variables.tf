1d773f6e8f303a4b696b847c5a49e80594c65f36
liorm <lior@email.com>
2023-09-16 08:47:03 +0300

    declaring variables in variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,5 +1,34 @@
 variable "AMI" {
 type = string
 description = "Image for ubuntu instance"
-default = "ami-053b0d53c279acc90"
+}
+
+variable "VPC_ID" {
+type = string
+description = "ID of my vpc"
+}
+
+variable "SG" {
+type = string
+description = "SG for liorm-TF-easy instances"
+}
+
+variable "RT_ID" {
+type = string
+description = "Route Table ID"
+}
+
+variable "SUBNET_1A" {
+type = string
+description = "us-east-1a subnet"
+}
+
+variable "SUBNET_1B" {
+type = string
+description = "us-east-1b subnet"
+}
+
+variable "TG_ARN" {
+type = string
+description = "Target Group Amazon Resource Name (ARN)"
 }
