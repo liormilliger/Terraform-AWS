c7e36eec5af08eb956c1018530188b718ad5b648
liorm <lior@email.com>
2023-09-16 09:38:18 +0300

    resetting vars in tf files, AMI var only - apply works

--- application.tf
+++ application.tf
@@ -2,17 +2,17 @@ resource "aws_lb_target_group" "liorm-TF-easy-ALB-TG" {
   name     = "liorm-TF-easy-ALB-TG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.VPC_ID}"
+  vpc_id   = aws_vpc.liorm-TF-easy-vpc.id
 }
 
 resource "aws_lb_target_group_attachment" "liorm-tg-attach-1a" {
-  target_group_arn = "${var.TG_ARN}"
+  target_group_arn = aws_lb_target_group.liorm-TF-easy-ALB-TG.arn
   target_id        = aws_instance.liorm-TF-easy-EC2-1a.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "liorm-tg-attach-1b" {
-  target_group_arn = "${var.TG_ARN}"
+  target_group_arn = aws_lb_target_group.liorm-TF-easy-ALB-TG.arn
   target_id        = aws_instance.liorm-TF-easy-EC2-1b.id
   port             = 80
 }
@@ -21,8 +21,9 @@ resource "aws_lb" "liorm-TF-ALB" {
   name               = "liorm-TF-ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = "${var.SG}"
-  subnets            = ["${var.SUBNET_1A}", "${var.SUBNET_1B}"]
+  security_groups    = [aws_security_group.liorm-TF-easy-SG.id]
+  subnets            = [aws_subnet.liorm-TF-easy-us-east-1a.id, aws_subnet.liorm-TF-easy-us-east-1b.id]
+
   enable_deletion_protection = false
 
   tags = {
