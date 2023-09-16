81449889b22998ecc3b7479dfd3cab3e555540cf
liorm <lior@email.com>
2023-09-16 08:52:13 +0300

    declaring variables in variables.tf file

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "liorm-TF-easy-EC2-1a" {
   key_name = aws_key_pair.liorm-tf-key.id
   subnet_id     = "${var.SUBNET_1A}" 
   availability_zone = "us-east-1a"
-  vpc_security_group_ids = ${var.SG}
+  vpc_security_group_ids = "${var.SG}"
 
   tags = {
     Name = "liorm-TF-easy-instance-1a"
@@ -17,7 +17,7 @@ resource "aws_instance" "liorm-TF-easy-EC2-1b" {
   key_name = aws_key_pair.liorm-tf-key.id
   subnet_id     = "${var.SUBNET_1B}"
   availability_zone = "us-east-1b"
-  vpc_security_group_ids = ${var.SG}
+  vpc_security_group_ids = "${var.SG}"
 
   tags = {
     Name = "liorm-TF-easy-instance-1b"
