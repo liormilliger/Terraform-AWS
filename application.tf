8cf773ae62587da21ecaf41c6035c0d72f4a5b1f
liorm <lior@email.com>
2023-09-16 08:20:59 +0300

    adding tfvar files and vars in all files

--- application.tf
+++ application.tf
@@ -2,17 +2,17 @@ resource "aws_lb_target_group" "liorm-TF-easy-ALB-TG" {
   name     = "liorm-TF-easy-ALB-TG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.liorm-TF-easy-vpc.id
+  vpc_id   = ${var.VPC}
 }
 
 resource "aws_lb_target_group_attachment" "liorm-tg-attach-1a" {
-  target_group_arn = aws_lb_target_group.liorm-TF-easy-ALB-TG.arn
+  target_group_arn = ${var.TG_ARN}
   target_id        = aws_instance.liorm-TF-easy-EC2-1a.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "liorm-tg-attach-1b" {
-  target_group_arn = aws_lb_target_group.liorm-TF-easy-ALB-TG.arn
+  target_group_arn = ${var.TG_ARN}
   target_id        = aws_instance.liorm-TF-easy-EC2-1b.id
   port             = 80
 }
@@ -21,9 +21,8 @@ resource "aws_lb" "liorm-TF-ALB" {
   name               = "liorm-TF-ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.liorm-TF-easy-SG.id]
-  subnets            = [aws_subnet.liorm-TF-easy-us-east-1a.id, aws_subnet.liorm-TF-easy-us-east-1b.id]
-
+  security_groups    = ${var.SG}
+  subnets            = [${var.SUBNET_1A}, ${var.SUBNET_1B}]
   enable_deletion_protection = false
 
   tags = {
@@ -38,6 +37,6 @@ resource "aws_lb_listener" "front_end" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.liorm-TF-easy-ALB-TG.arn
+    target_group_arn = ${var.TG_ARN}
   }
 }
--- instances.tf
+++ instances.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "liorm-TF-easy-EC2-1a" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = ${var.AMI}
   instance_type = "t3.micro"
   key_name = aws_key_pair.liorm-tf-key.id
-  subnet_id     = aws_subnet.liorm-TF-easy-us-east-1a.id 
+  subnet_id     = ${var.SUBNET_1A} 
   availability_zone = "us-east-1a"
-  vpc_security_group_ids = [aws_security_group.liorm-TF-easy-SG.id]
+  vpc_security_group_ids = ${var.SG}
 
   tags = {
     Name = "liorm-TF-easy-instance-1a"
@@ -12,12 +12,12 @@ resource "aws_instance" "liorm-TF-easy-EC2-1a" {
 }
 
 resource "aws_instance" "liorm-TF-easy-EC2-1b" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = ${var.AMI}
   instance_type = "t3.micro"
   key_name = aws_key_pair.liorm-tf-key.id
-  subnet_id     = aws_subnet.liorm-TF-easy-us-east-1b.id 
+  subnet_id     = ${var.SUBNET_1B} 
   availability_zone = "us-east-1b"
-  vpc_security_group_ids = [aws_security_group.liorm-TF-easy-SG.id]
+  vpc_security_group_ids = ${var.SG}
 
   tags = {
     Name = "liorm-TF-easy-instance-1b"
